fabc9d23452f8fa1536f9c19e8a7fd02c3cd6f56
snehasonwane <snehasonwane01@gmail.com>
2022-05-21 13:55:26 +0530

    [Manasi] Add: sad Mood Return Sad

---
+++ MSTestMoodAnalyser/MoodAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MSTestMoodAnalyser
+{
+    internal class MoodAnalyzer
+    {
+        public MoodAnalyzer(string v)
+        {
+        }
+
+        internal string Analyser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("----Welcome To Mood Analyser----");
+            //Console.Read();
+        }
+    }
+
+}
